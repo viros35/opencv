24163fdd0ebe593a7ea699c06a3e27c4d737a662
viros35 <kuligin.viktor.alexandrovich@gmail.com>
2025-01-24 14:23:15 +0300

    commit_4

--- src/opencv_pkg/opencv_pkg/opencv_1_rx.py
+++ src/opencv_pkg/opencv_pkg/opencv_1_rx.py
@@ -21,6 +21,7 @@ class ReadTopicImg(Node):
         img = cv2.rectangle(img, (20,20), (200,100), (255,255,255), 5)
         img = cv2.circle(img, (200,200),30,(255,255,255),5,8,0)
         img = cv2.line(img, (40,100), (200,200), (255,255,255), 5)
+        
 
         self.get_logger().info('img read',throttle_duration_sec = 1)
         cv2.imshow('camera', img)
